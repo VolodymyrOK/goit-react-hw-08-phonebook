dfce793dcda611a0a18bc74ec5f6c55e2ea643a1
Volodymyr <vladikon70@gmail.com>
2023-10-21 21:57:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider theme={Theme}>
-            <BrowserRouter basename="phonebook-kv.netlify.app">
+            <BrowserRouter>
               <App />
             </BrowserRouter>
           </ThemeProvider>
