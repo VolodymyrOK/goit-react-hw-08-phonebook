e3c517518853539214d491ae457658086357d98d
Volodymyr <vladikon70@gmail.com>
2023-10-21 22:06:27 +0200

    update files

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider theme={Theme}>
-            <BrowserRouter>
+            <BrowserRouter basename="/goit-react-hw-08-phonebook">
               <App />
             </BrowserRouter>
           </ThemeProvider>
